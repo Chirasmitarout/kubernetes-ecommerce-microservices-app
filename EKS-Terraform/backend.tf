8e8afa6784023edcb1df158bf899ce93ffd0fb92
SuprabhaNayak93 <rojirenu5@gmail.com>
2026-09-19 14:26:26 +0530

    Update backend.tf

--- EKS-Terraform/backend.tf
+++ EKS-Terraform/backend.tf
@@ -1,6 +1,6 @@
 terraform {
   backend "s3" {
-    bucket       = "suprojiookkbbnnvvssddff"
+    bucket       = "supjjggrojikkknayak34ff"
     key          = "terraform.tfstate"
     region       = "us-east-1"
     use_lockfile = true
